425373545fe330a43ff6515dd639307b672c3340
infernostars <blazem7708.2@gmail.com>
2023-02-27 12:23:34 -0700

    Use a Component instead of a string, since it's depreciated in Paper

--- src/main/java/dev/infernity/polarisplugin/polarisplugin/listeners/onPlayerJoinListener.kt
+++ src/main/java/dev/infernity/polarisplugin/polarisplugin/listeners/onPlayerJoinListener.kt
@@ -1,6 +1,7 @@
 package dev.infernity.polarisplugin.polarisplugin.listeners
 
 import dev.infernity.polarisplugin.polarisplugin.PolarisPlugin
+import net.kyori.adventure.text.Component
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerJoinEvent
@@ -8,7 +9,7 @@ import org.bukkit.event.player.PlayerJoinEvent
 class onPlayerJoinListener : Listener {
     @EventHandler
     fun onPlayerJoin(event: PlayerJoinEvent) {
-        event.joinMessage = event.player.name + " joined Polaris!"
+        event.joinMessage(Component.text(event.player.name + " joined Polaris!"))
         event.player.discoverRecipe(PolarisPlugin.Companion.createNamespacedKey("ice_cream_vanilla"))
     }
 }
